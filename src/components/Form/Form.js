5cbc2344783a1d2a2ffd027cc756bc01b592d181
Sukhjinder Arora <sukhb1995@gmail.com>
2019-01-04 19:49:17 +0530

    Added type submit to form button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,14 +3,14 @@ import style from './Form.module.css';
 
 const Form = (props) => {
   return (
-    <form  className={style.form} onSubmit={props.formSubmit}>
+    <form className={style.form} onSubmit={props.formSubmit}>
       <input 
         className={style.taskInput} 
         value={props.text} 
         placeholder="Enter Task" 
         onChange={props.changeHandler}
       />
-      <button className={style.btn}>Add Task</button>
+      <button className={style.btn} type="submit">Add Task</button>
     </form>
   );
 }
